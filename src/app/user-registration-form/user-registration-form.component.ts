76583172a7152e831274ae7ad0a5db225079c6ac
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-02-24 14:25:33 +0100

    Create UserRegistrationFormComponent and connect to logic in fetchApiDataService
    Import this component to app.component.ts to create dialog function to call and then import to app.component.html for rendering

---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { of } from 'rxjs';
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss'],
+})
+export class UserRegistrationFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  // This is the function responsible for sending the form inputs to the backend
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe(
+      (response) => {
+        //Logic for a succesful user registration goes here! (To be implemented)
+        this.dialogRef.close(); // This will close the modal on success!
+        console.log(response);
+        this.snackBar.open('user registered succesfully', 'OK', {
+          duration: 2000,
+        });
+      },
+      (response) => {
+        console.log(response);
+        this.snackBar.open(response, 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+}
+
+/* (method) Observable<any>.subscribe(next?: ((value: any) => void) | null | undefined, error?: ((error: any) => void) | null | undefined, complete?: (() => void) | null | undefined):  */
