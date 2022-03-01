01d456b96f7e8e5bfaed2b306914f70ab006cc17
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-03-01 12:34:01 +0100

    create navigationBar component files

---
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation-bar',
+  templateUrl: './navigation-bar.component.html',
+  styleUrls: ['./navigation-bar.component.scss'],
+})
+export class NavigationBarComponent implements OnInit {
+  constructor(public router: Router) {}
+
+  ngOnInit(): void {}
+
+  openProfileView(): void {
+    this.router.navigate(['profile']);
+  }
+  openMovieView(): void {
+    this.router.navigate(['movies']);
+  }
+  logOutUser(): void {
+    const endSession = localStorage.clear();
+    this.router.navigate(['welcome']);
+    return endSession;
+  }
+}
