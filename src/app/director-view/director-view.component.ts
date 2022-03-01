cc789205d3f9d131542b9084ef890813ba2120b2
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-03-01 12:27:10 +0100

    Create Director-view component files

---
+++ src/app/director-view/director-view.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-director-view',
+  templateUrl: './director-view.component.html',
+  styleUrls: ['./director-view.component.scss'],
+})
+export class DirectorViewComponent implements OnInit {
+  constructor(
+    /**
+     * Use Inject to get the movie details from the movie object
+     */
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string;
+      Bio: string;
+    }
+  ) {}
+
+  ngOnInit(): void {}
+}
